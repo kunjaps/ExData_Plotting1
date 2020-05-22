7da1bd498ff10e97fa1b95c45f5f3a6bfc5ca10a
kunjaps <kunjapee@gmail.com>
2020-05-23 03:40:45 +0530

    further push

--- plot1.R
+++ plot1.R
@@ -10,7 +10,7 @@ blue <- read.csv2("household_power_consumption.txt")
 green <- subset(blue, dmy(blue$Date)==dmy("2-2-2007")|dmy(blue$Date)==dmy("1-2-2007"))
 
 #plotting 
-hist(as.numeric(as.character(green$Global_active_power)), xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+hist(as.numeric(as.character(green$Global_active_power)), xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "RED")
 
 #saving the file
 dev.copy(png, file = "plot1.png",height=480, width=480)
